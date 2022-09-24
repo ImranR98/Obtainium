540000485cb4d12f7640285f802e76fe083c1e91
Imran Remtulla <imranr98@pm.me>
2022-09-23 23:55:36 -0400

    Changed a default

--- lib/app_sources/github.dart
+++ lib/app_sources/github.dart
@@ -123,5 +123,5 @@ class GitHub implements AppSource {
   ];
 
   @override
-  List<String> additionalDataDefaults = ["true", "", ""];
+  List<String> additionalDataDefaults = ["true", "true", ""];
 }
