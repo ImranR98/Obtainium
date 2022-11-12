e2bf83498182ae789676c470a48734c7a343ac98
Imran Remtulla <imranr98@pm.me>
2022-11-12 02:15:23 -0500

    Increment version

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ import 'package:dynamic_color/dynamic_color.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 
-const String currentVersion = '0.6.11';
+const String currentVersion = '0.7.0';
 const String currentReleaseTag =
     'v$currentVersion-beta'; // KEEP THIS IN SYNC WITH GITHUB RELEASES
 
