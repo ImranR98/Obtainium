e888fb712099ba2c1144b1b8eb0897c5a8b639e4
Imran Remtulla <imranr98@pm.me>
2023-01-07 16:49:38 -0500

    Don't skip installing same-versionCode updates

--- lib/providers/apps_provider.dart
+++ lib/providers/apps_provider.dart
@@ -247,10 +247,7 @@ class AppsProvider with ChangeNotifier {
         !(await canDowngradeApps())) {
       throw DowngradeError();
     }
-    if (appInfo == null ||
-        int.parse(newInfo.buildNumber) > appInfo.versionCode!) {
-      await InstallPlugin.installApk(file.file.path, 'dev.imranr.obtainium');
-    }
+    await InstallPlugin.installApk(file.file.path, 'dev.imranr.obtainium');
     apps[file.appId]!.app.installedVersion =
         apps[file.appId]!.app.latestVersion;
     // Don't correct install status as installation may not be done yet
