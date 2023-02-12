fd9aebc5b2340856d6fd00e3589e5d01e49d5717
Imran Remtulla <imranr98@pm.me>
2023-02-12 14:36:54 -0500

    Reverse changes related to App ID changes (#270)

--- lib/custom_errors.dart
+++ lib/custom_errors.dart
@@ -44,6 +44,10 @@ class DowngradeError extends ObtainiumError {
   DowngradeError() : super(tr('cantInstallOlderVersion'));
 }
 
+class IDChangedError extends ObtainiumError {
+  IDChangedError() : super(tr('appIdMismatch'));
+}
+
 class NotImplementedError extends ObtainiumError {
   NotImplementedError() : super(tr('functionNotImplemented'));
 }
--- lib/providers/apps_provider.dart
+++ lib/providers/apps_provider.dart
@@ -179,9 +179,12 @@ class AppsProvider with ChangeNotifier {
       }
     }
     // If the APK package ID is different from the App ID, it is either new (using a placeholder ID) or the ID has changed
-    // In either case, the app should be given the new ID
+    // The former case should be handled (give the App its real ID), the latter is a security issue
     var newInfo = await PackageArchiveInfo.fromPath(downloadedFile.path);
     if (app.id != newInfo.packageName) {
+      if (apps[app.id] != null && !SourceProvider().isTempId(app.id)) {
+        throw IDChangedError();
+      }
       var originalAppId = app.id;
       app.id = newInfo.packageName;
       downloadedFile = downloadedFile.renameSync(
