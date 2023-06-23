ff5152bf7957fa70f4e2dbf428db294d5bd63950
Imran Remtulla <imranr98@pm.me>
2023-06-23 12:07:34 -0400

    Added RU language menu entry

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ List<MapEntry<Locale, String>> supportedLocales = const [
   MapEntry(Locale('fr'), 'Français'),
   MapEntry(Locale('es'), 'Español'),
   MapEntry(Locale('pl'), 'Polski'),
+  MapEntry(Locale('ru'), 'Русский язык'),
 ];
 const fallbackLocale = Locale('en');
 const localeDir = 'assets/translations';
