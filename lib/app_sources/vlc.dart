a2571e61a85d043c2e8700e5524a0cc0df087443
Imran Remtulla <imranr98@pm.me>
2023-08-19 01:28:25 -0400

    Fix VLC Source (#758)

--- lib/app_sources/vlc.dart
+++ lib/app_sources/vlc.dart
@@ -1,5 +1,6 @@
 import 'package:html/parser.dart';
 import 'package:http/http.dart';
+import 'package:obtainium/app_sources/html.dart';
 import 'package:obtainium/custom_errors.dart';
 import 'package:obtainium/providers/source_provider.dart';
 
@@ -7,10 +8,32 @@ class VLC extends AppSource {
   VLC() {
     host = 'videolan.org';
   }
+  get dwUrlBase => 'https://get.$host/vlc-android/';
+
+  @override
+  Map<String, String>? get requestHeaders => HTML().requestHeaders;
 
   @override
   String sourceSpecificStandardizeURL(String url) {
     return 'https://$host';
+  }
+
+  Future<String?> getLatestVersion(String standardUrl) async {
+    Response res = await sourceRequest(dwUrlBase);
+    if (res.statusCode == 200) {
+      var dwLinks = parse(res.body)
+          .querySelectorAll('a')
+          .where((element) => element.attributes['href'] != 'last/')
+          .map((e) => e.attributes['href']?.split('/')[0])
+          .toList();
+      String? version = dwLinks.isNotEmpty ? dwLinks.last : null;
+      if (version == null) {
+        throw NoVersionError();
+      }
+      return version;
+    } else {
+      throw getObtainiumHttpError(res);
+    }
   }
 
   @override
@@ -18,44 +41,27 @@ class VLC extends AppSource {
     String standardUrl,
     Map<String, dynamic> additionalSettings,
   ) async {
-    Response res = await sourceRequest(
-        'https://www.videolan.org/vlc/download-android.html');
+    String? version = await getLatestVersion(standardUrl);
+    if (version == null) {
+      throw NoVersionError();
+    }
+    String? targetUrl = '$dwUrlBase$version/';
+    Response res = await sourceRequest(targetUrl);
+    List<String> apkUrls = [];
     if (res.statusCode == 200) {
-      var dwUrlBase = 'get.videolan.org/vlc-android';
-      var dwLinks = parse(res.body)
+      apkUrls = parse(res.body)
           .querySelectorAll('a')
-          .where((element) =>
-              element.attributes['href']?.contains(dwUrlBase) ?? false)
+          .map((e) => e.attributes['href']?.split('/').last)
+          .where((h) =>
+              h != null && h.isNotEmpty && h.toLowerCase().endsWith('.apk'))
+          .map((e) => targetUrl + e!)
           .toList();
-      String? version = dwLinks.isNotEmpty
-          ? dwLinks.first.attributes['href']
-              ?.split('/')
-              .where((s) => s.isNotEmpty)
-              .last
-          : null;
-      if (version == null) {
-        throw NoVersionError();
-      }
-      String? targetUrl = 'https://$dwUrlBase/$version/';
-      Response res2 = await sourceRequest(targetUrl);
-      List<String> apkUrls = [];
-      if (res2.statusCode == 200) {
-        apkUrls = parse(res2.body)
-            .querySelectorAll('a')
-            .map((e) => e.attributes['href']?.split('/').last)
-            .where((h) =>
-                h != null && h.isNotEmpty && h.toLowerCase().endsWith('.apk'))
-            .map((e) => targetUrl + e!)
-            .toList();
-      } else {
-        throw getObtainiumHttpError(res2);
-      }
-
-      return APKDetails(
-          version, getApkUrlsFromUrls(apkUrls), AppNames('VideoLAN', 'VLC'));
     } else {
       throw getObtainiumHttpError(res);
     }
+
+    return APKDetails(
+        version, getApkUrlsFromUrls(apkUrls), AppNames('VideoLAN', 'VLC'));
   }
 
   @override
