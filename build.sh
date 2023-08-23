5eac851f8014621e18a5e7dfc440d15c97791a1d
Imran Remtulla <imranr98@pm.me>
2023-08-23 19:36:24 -0400

    Added convenience build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Convenience script
+
+git fetch && git merge origin/main && git push; # Typically run after a PR to main, so bring dev up to date
+rm ./build/app/outputs/flutter-apk/* 2>/dev/null; # Get rid of older builds if any
+flutter build apk && flutter build apk --split-per-abi # Build (both split and combined APKs)
+for file in ./build/app/outputs/flutter-apk/*.sha1; do gpg --sign --detach-sig "$file"; done # Generate PGP signatures
+rsync -r ./build/app/outputs/flutter-apk/ ~/Downloads/Obtainium-build/ # Dropoff in Downloads to allow for drag-drop into Flatpak Firefox
