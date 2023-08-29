4ba3f7996ed5ae0725f6c82d7b5aa98e835b4760
Imran Remtulla <imranr98@pm.me>
2023-08-28 22:08:37 -0400

    Increment version

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ import 'package:easy_localization/src/easy_localization_controller.dart';
 // ignore: implementation_imports
 import 'package:easy_localization/src/localization.dart';
 
-const String currentVersion = '0.14.2';
+const String currentVersion = '0.14.3';
 const String currentReleaseTag =
     'v$currentVersion-beta'; // KEEP THIS IN SYNC WITH GITHUB RELEASES
 
