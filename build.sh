05f497787ee87aec018a230bee84616ee721864e
Imran Remtulla <imranr98@pm.me>
2023-09-10 22:37:32 -0400

    Build script tweak

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@
 CURR_DIR="$(pwd)"
 trap "cd "$CURR_DIR"" EXIT
 
-git fetch && git merge origin/main && git push                                               # Typically run after a PR to main, so bring dev up to date
+if [ -z "$1" ]; then
+    git fetch && git merge origin/main && git push # Typically run after a PR to main, so bring dev up to date
+fi
 rm ./build/app/outputs/flutter-apk/* 2>/dev/null                                             # Get rid of older builds if any
 flutter build apk && flutter build apk --split-per-abi                                       # Build (both split and combined APKs)
 for file in ./build/app/outputs/flutter-apk/*.sha1; do gpg --sign --detach-sig "$file"; done # Generate PGP signatures
