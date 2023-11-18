cd2857cb944f7cfcb82a8e48dcc156dde029c5dd
Imran Remtulla <imranr98@pm.me>
2023-11-18 15:56:44 -0500

    Add language menu entries (+ tr fix), increment version

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ import 'package:easy_localization/src/easy_localization_controller.dart';
 // ignore: implementation_imports
 import 'package:easy_localization/src/localization.dart';
 
-const String currentVersion = '0.14.32';
+const String currentVersion = '0.14.33';
 const String currentReleaseTag =
     'v$currentVersion-beta'; // KEEP THIS IN SYNC WITH GITHUB RELEASES
 
@@ -42,6 +42,8 @@ List<MapEntry<Locale, String>> supportedLocales = const [
   MapEntry(Locale('cs'), 'Česky'),
   MapEntry(Locale('sv'), 'Svenska'),
   MapEntry(Locale('nl'), 'Nederlands'),
+  MapEntry(Locale('vi'), 'Tiếng Việt'),
+  MapEntry(Locale('tr'), 'Türkçe'),
 ];
 const fallbackLocale = Locale('en');
 const localeDir = 'assets/translations';
