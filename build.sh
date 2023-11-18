b3ddb8658f6b55fcd4c1214a3892f4efdf6712c2
Imran Remtulla <imranr98@pm.me>
2023-11-18 15:42:32 -0500

    F-Droid flavour updates + update packages

--- build.sh
+++ build.sh
@@ -7,8 +7,11 @@ trap "cd "$CURR_DIR"" EXIT
 if [ -z "$1" ]; then
     git fetch && git merge origin/main && git push # Typically run after a PR to main, so bring dev up to date
 fi
-rm ./build/app/outputs/flutter-apk/* 2>/dev/null                                             # Get rid of older builds if any
-flutter build apk && flutter build apk --split-per-abi                                       # Build (both split and combined APKs)
+rm ./build/app/outputs/flutter-apk/* 2>/dev/null                                       # Get rid of older builds if any
+flutter build apk --flavor normal && flutter build apk --split-per-abi --flavor normal # Build (both split and combined APKs)
+for file in ./build/app/outputs/flutter-apk/app-*normal*.apk*; do mv "$file" "${file//-normal/}"; done
+flutter build apk --flavor fdroid -t lib/main_fdroid.dart && # Do the same for the F-Droid flavour
+    flutter build apk --split-per-abi --flavor fdroid -t lib/main_fdroid.dart
 for file in ./build/app/outputs/flutter-apk/*.sha1; do gpg --sign --detach-sig "$file"; done # Generate PGP signatures
 rsync -r ./build/app/outputs/flutter-apk/ ~/Downloads/Obtainium-build/                       # Dropoff in Downloads to allow for drag-drop into Flatpak Firefox
 cd ~/Downloads/Obtainium-build/                                                              # Make zips just in case (for in-comment uploads)
