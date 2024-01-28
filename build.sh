c9751227a5cb87648e3d877bd8699e351e72751b
Imran Remtulla <imranr98@pm.me>
2024-01-27 21:56:26 -0500

    Flutter upgrade + versionCode changes + increment version

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ if [ -z "$1" ]; then
     git fetch && git merge origin/main && git push # Typically run after a PR to main, so bring dev up to date
 fi
 cd .flutter
+git fetch
 git checkout "$(flutter --version | head -2 | tail -1 | awk '{print $4}')" # Ensure included Flutter submodule version equals my environment
 cd ..
 rm ./build/app/outputs/flutter-apk/* 2>/dev/null                                       # Get rid of older builds if any
