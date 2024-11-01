cc373c8d7f246bce5dd73e11168b49787f47e0a7
Imran Remtulla <contact@imranr.dev>
2024-11-01 14:09:38 -0400

    Remove Signal source (HTML is enough) (#1928)

--- lib/providers/source_provider.dart
+++ lib/providers/source_provider.dart
@@ -23,7 +23,6 @@ import 'package:obtainium/app_sources/izzyondroid.dart';
 import 'package:obtainium/app_sources/html.dart';
 import 'package:obtainium/app_sources/jenkins.dart';
 import 'package:obtainium/app_sources/neutroncode.dart';
-import 'package:obtainium/app_sources/signal.dart';
 import 'package:obtainium/app_sources/sourceforge.dart';
 import 'package:obtainium/app_sources/sourcehut.dart';
 import 'package:obtainium/app_sources/steammobile.dart';
@@ -198,6 +197,23 @@ appJSONCompatibilityModifiers(Map<String, dynamic> json) {
       replacementAdditionalSettings['versionExtractionRegEx'] =
           replacementAdditionalSettings['customLinkFilterRegex'];
       replacementAdditionalSettings['matchGroupToUse'] = '\$1';
+      additionalSettings = replacementAdditionalSettings;
+    }
+    // Signal apps from before it was removed should be converted to HTML (#1928)
+    if (json['url'] == 'https://signal.org' &&
+        json['id'] == 'org.thoughtcrime.securesms' &&
+        json['author'] == 'Signal' &&
+        json['name'] == 'Signal' &&
+        json['overrideSource'] == null &&
+        additionalSettings['trackOnly'] == false &&
+        additionalSettings['versionExtractionRegEx'] == '' &&
+        json['lastUpdateCheck'] != null &&
+        json['lastUpdateCheck'] <= 1730484400402000) {
+      json['url'] = 'https://updates.signal.org/android/latest.json';
+      var replacementAdditionalSettings = getDefaultValuesFromFormItems(
+          HTML().combinedAppSpecificSettingFormItems);
+      replacementAdditionalSettings['versionExtractionRegEx'] =
+          '\\d+.\\d+.\\d+';
       additionalSettings = replacementAdditionalSettings;
     }
   }
@@ -794,7 +810,6 @@ class SourceProvider {
         Tencent(),
         Jenkins(),
         APKMirror(),
-        Signal(),
         VLC(),
         WhatsApp(),
         TelegramApp(),
