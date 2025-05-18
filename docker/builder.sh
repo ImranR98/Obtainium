195c0d1f113d2a3bf3f5255594f4d5dddae09ff4
Imran Remtulla <contact@imranr.dev>
2025-05-17 21:56:32 -0400

    Update Kotlin version (to enable Docker builder) + update Dockerfile

--- docker/builder.sh
+++ docker/builder.sh
@@ -8,11 +8,11 @@ docker run \
     --rm \
     -ti \
     --net host \
-    -v "${PWD}/../:${PWD}/../" \
+    -v "${PWD}/../:${PWD}/../":z \
     -w "${PWD}" \
     --name flutter-dev-obtainium \
     --user $(id -u) \
-    -v ./data/home:/home/${USER} \
+    -v ./data/home:/home/${USER}:z \
     -e USER=${USER} \
     -e HOME=/home/${USER} \
     -e ANDROID_USER_HOME=${HOME}/.android \
