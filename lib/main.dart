3890785fc7e3d9b02a72c6cb34a07c25c24293df
Imran Remtulla <contact@imranr.dev>
2025-05-19 15:19:34 -0400

    Add Arabic to language menu

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,7 @@ List<MapEntry<Locale, String>> supportedLocales = const [
   MapEntry(Locale('in'), 'Bahasa Indonesia'),
   MapEntry(Locale('ko'), '한국어'),
   MapEntry(Locale('ca'), 'Català'),
+  MapEntry(Locale('ar'), 'عربى'),
 ];
 const fallbackLocale = Locale('en');
 const localeDir = 'assets/translations';
