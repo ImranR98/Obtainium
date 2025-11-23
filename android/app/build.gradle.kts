b72ba4394cb48cae177d8b58f6dd73b71ed3a3f0
deminearchiver <minearchiver@gmail.com>
2025-11-23 07:15:28 +0300

    Upgraded project to Flutter 3.38.3. - Bumped Dart SDK to 3.10.1 - Bumped Flutter SDK to 3.38.0 - Bumped NDK to 28.2.13676358 - Bumped com.android.application to 8.11.1 - Bumped org.jetbrains.kotlin.android to 2.2.20 - Bumped Java to 17 - Changed formatting in Gradle configuration files to align with a fresh Flutter project's

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,16 +30,16 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "dev.imranr.obtainium"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" // 'flutter.ndkVersion' produces warnings (TODO can/should we switch back?)
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
