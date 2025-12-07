0834312496c8c147a392ec0e71440cd15e30eb2b
Imran Remtulla <contact@imranr.dev>
2025-12-06 23:50:11 -0500

    Better link extraction (#2627)

--- lib/app_sources/html.dart
+++ lib/app_sources/html.dart
@@ -102,7 +102,7 @@ bool _isNumeric(String s) {
 
 List<MapEntry<String, String>> getLinksInLines(String lines) =>
     RegExp(
-          r'(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?',
+          '(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?',
         )
         .allMatches(lines)
         .map(
@@ -249,7 +249,7 @@ class HTML extends AppSource {
     [
       GeneratedFormSwitch(
         'matchLinksOutsideATags',
-        label: tr('matchLinksOutsideATags')
+        label: tr('matchLinksOutsideATags'),
       ),
     ],
     [GeneratedFormSwitch('skipSort', label: tr('skipSort'))],
