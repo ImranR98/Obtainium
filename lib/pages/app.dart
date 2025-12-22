306d18c55cd2b02ce9699f834aa82500a9d78cce
Giannis Lagodimos <62063308+lagodimos@users.noreply.github.com>
2025-12-22 23:24:14 +0200

    Show certificate hash(es) on app page

--- lib/pages/app.dart
+++ lib/pages/app.dart
@@ -1,3 +1,4 @@
+import 'package:crypto/crypto.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -14,6 +15,7 @@ import 'package:url_launcher/url_launcher_string.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:markdown/markdown.dart' as md;
+import 'package:android_package_manager/android_package_manager.dart' hide LaunchMode;
 
 class AppPage extends StatefulWidget {
   const AppPage({
@@ -34,6 +36,36 @@ class _AppPageState extends State<AppPage> {
   bool _wasWebViewOpened = false;
   AppInMemory? prevApp;
   bool updating = false;
+  bool hasMultipleSigners = false;
+  List<String> certificateHashes = [];
+
+  Future<void> _loadAppHashes() async {
+    final AndroidPackageManager pm = AndroidPackageManager();
+
+    final packageInfo = await pm.getPackageInfo(
+      packageName: prevApp?.app.id as String,
+      flags: PackageInfoFlags({PMFlag.getSigningCertificates})
+    );
+
+    final multipleSigners = packageInfo?.signingInfo?.hasMultipleSigners ?? false;
+
+    // https://developer.android.com/reference/android/content/pm/SigningInfo#getApkContentsSigners()
+    final signatures = hasMultipleSigners ?
+        packageInfo?.signingInfo?.apkContentSigners :
+        packageInfo?.signingInfo?.signingCertificateHistory;
+
+    final hashes = signatures?.map((signature) {
+      final digest = sha256.convert(signature);
+      return digest.bytes
+          .map((b) => b.toRadixString(16).padLeft(2, '0').toUpperCase())
+          .join(':');
+    }).toList() ?? [];
+
+    setState(() {
+      hasMultipleSigners = multipleSigners;
+      certificateHashes = hashes;
+    });
+  }
 
   @override
   void initState() {
@@ -110,6 +142,7 @@ class _AppPageState extends State<AppPage> {
         app != null &&
         settingsProvider.checkUpdateOnDetailPage) {
       prevApp = app;
+      _loadAppHashes();
       getUpdate(app.app.id);
     }
     var trackOnly = app?.app.additionalSettings['trackOnly'] == true;
@@ -256,6 +289,42 @@ class _AppPageState extends State<AppPage> {
                 ],
               ),
             ),
+
+          /* Certificate Hashes */
+          if (installed && certificateHashes.isNotEmpty) Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 48),
+              Text(
+                "${plural('certificateHash', certificateHashes.length)}"
+                    "${hasMultipleSigners ? " (${tr('multipleSigners')})" : ""}",
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 13),
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: certificateHashes.map((hash) {
+                  return GestureDetector(
+                    onLongPress: () {
+                      Clipboard.setData(ClipboardData(text: hash));
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text(tr('copiedToClipboard'))),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25,vertical: 5),
+                      child: Text(
+                        hash,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+
           const SizedBox(height: 48),
           CategoryEditorSelector(
             alignment: WrapAlignment.center,
