eeca654059ecca73dfee8dde67bd278ee5408a3b
Imran Remtulla <contact@imranr.dev>
2026-08-05 18:43:02 -0400

    Fix pinUpdates: treat uncertain version comparison as update, not non-update (#3078 followup)

--- lib/providers/apps_provider_updates.dart
+++ lib/providers/apps_provider_updates.dart
@@ -266,7 +266,7 @@ extension AppsProviderUpdates on AppsProvider {
                   app.settings.getBool('versionDetection')) {
                 final isNewer = VersionService()
                     .isVersionNewer(app.installedVersion!, app.latestVersion);
-                if (isNewer == true) {
+                if (isNewer != false) {
                   updateAppIds.add(app.id);
                 }
               } else {
